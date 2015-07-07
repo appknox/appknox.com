8868f1739a777cf0aafbd5a63809c531ce2cd016
dhilipsiva <dhilipsiva@gmail.com>
2015-07-07 13:56:54 +0530

    Deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#
+# deploy.sh
+# Copyright (C) 2015 dhilipsiva <dhilipsiva@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+#! /bin/sh
+#
+# deploy.sh
+# Copyright (C) 2014 dhilipsiva <dhilipsiva@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+jekyll build
+aws s3 sync _site/ s3://appknox.com/ --acl public-read
